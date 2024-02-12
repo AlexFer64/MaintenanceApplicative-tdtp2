748d54e88a586a7210d911513734d5e15027749b
alexandre.ferreira <alexandre.ferreira@hupi.fr>
2024-02-12 15:01:33 +0100

    Arrêt au timer de la vidéo: 16 min 27 s

--- src/main/java/movierental/Rental.java
+++ src/main/java/movierental/Rental.java
@@ -13,6 +13,38 @@ public class Rental {
         _daysRented = daysRented;
     }
 
+    static double determineAmounts(Rental rental) {
+        double thisAmount = 0;
+
+        //determine amounts for each line
+        switch (rental.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (rental.getDaysRented() > 2)
+                    thisAmount += (rental.getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += rental.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (rental.getDaysRented() > 3)
+                    thisAmount += (rental.getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
+    }
+
+    static int addBonusForATwoDaysNewReleaseRental(Rental rental) {
+        if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1)
+            return 1;
+        return 0;
+    }
+
+    static int addFrequentRenterPoints(Rental rental) {
+        return addBonusForATwoDaysNewReleaseRental(rental) + 1;
+    }
+
     public int getDaysRented() {
         return _daysRented;
     }
